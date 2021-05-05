ebedd860b721fbe125329a388d743509321438e2
type <type314@hotmail.com>
2021-05-05 23:33:35 +0800

    add sunday algo

--- test/algo_test.go
+++ test/algo_test.go
@@ -83,3 +83,40 @@ func GetRandom(head *entity.ListNode) int {
 	}
 	return res
 }
+
+// 字符串匹配，Sunday算法，平均时间复杂度o(n)
+// https://cloud.tencent.com/developer/article/1683667?from=information.detail.sunday%E7%AE%97%E6%B3%95
+func StrStr(haystack, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	m := make(map[byte]int)
+	//记录needle中每个字符出现的位置
+	for i := range needle {
+		m[needle[i]] = i 
+	}
+	for i := 0; i <= len(haystack); {
+		tmpi := i
+		j := 0
+		for ;j < len(needle) && tmpi < len(haystack); j++ {
+			if haystack[tmpi] != needle[j] {
+				break
+			}
+			tmpi++
+		}
+		//匹配成功
+		if j == len(needle) {
+			return i
+		}
+		if i + len(needle) >= len(haystack) {
+			return -1
+		}
+		hit, exists := m[haystack[i+len(needle)]]
+		if exists {
+			i += len(needle) - hit
+		}else {
+			i +=len(needle) + 1
+		}
+	}
+	return -1
+}
